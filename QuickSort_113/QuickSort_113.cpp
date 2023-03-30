706aa44088bd80cbc9cbff66f3dd8607b544c098
rauda08 <raudajnnh08@gmail.com>
2023-03-30 16:09:33 +0700

    penambahan tititk dua pada line 53

--- QuickSort_113/QuickSort_113.cpp
+++ QuickSort_113/QuickSort_113.cpp
@@ -50,9 +50,9 @@ void q_short(int low, int high)
     //one containing elements less that or equal to pivot
     //outher conntaining elements greather than pivot
 
-    pivot = arr[low]                //Langkah Algoritma No. 2
+    pivot = arr[low];               //Langkah Algoritma No. 2
 
-        i = low + 1;                //Langkah Algoritma No. 3
+    i = low + 1;                //Langkah Algoritma No. 3
     j = high;                   //Langkah Algoritma No. 4
 
 
@@ -111,34 +111,12 @@ void display() {
     cout << "Number of data movements: " << mov_count << endl;
 } 
 
-int main ()
+int main()
 {
     input();
     q_short(0, n - 1);                  //Sort the array using quick sort
     display();
     system("pause");
 
-
-
-
-
-
-
-
+    return 0;
 }
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
