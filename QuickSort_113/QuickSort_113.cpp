4c1bc33672e3f5ffe3447f363ce04ba53f3db9af
rauda08 <raudajnnh08@gmail.com>
2023-03-30 15:09:14 +0700

    Add project files.

---
+++ QuickSort_113/QuickSort_113.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[20];        //array of integers to hold velues
+int cmp_count = 0;  //number of comparision
+int mov_count = 0;  //number of data movements
+int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum"
+    }
+
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">"
+            cint >> arr[i];
+    }
+}
+
+//swaps the element st index
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
